bd7184d1476d3b70d3e214087d6c70c9a2d8eec6
mayk93 <mihai.mandrescu@gmail.com>
2016-04-22 00:26:03 +0100

    Creating on remote

--- daily_commit.py
+++ daily_commit.py
@@ -15,6 +15,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Libs
+import pymongo
 import os
 import random
 import github
@@ -23,6 +24,20 @@ import github
 
 # Constants
 LANGUAGES = ["python", "python", "python", "go", "javascript"]  # More python to increase python probability
+
+def get_token():
+    client = pymongo.MongoClient()
+    return client.tokendb.token.find_one()["token"]
+
+TOKEN = get_token()
+USERNAME = "mayk93"   # Here goes your username
+REMOTE_PATH = "git@github.com:" + USERNAME + "/"
+
+'''
+git remote rename origin upstream
+git remote add origin URL_TO_GITHUB_REPO
+git push origin master
+'''
 
 
 def choose_repo(result):
@@ -44,11 +59,16 @@ def clone(url, name):
     os.system("cd ClonedRepos/" + name + "; git clone " + url)
 
 
+def create_repo_on_remote(name):
+    os.system("""curl -H "Authorization: token %s" --data '{"name":"%s"}' https://api.github.com/user/repos""" % (TOKEN, name))
+
+
 def main():
     client = github.Github()
     result = client.search_repositories(query=get_query(), sort="stars", order="desc")
     repo_url, repo_name = choose_repo(result)
     clone(repo_url, repo_name)
+    create_repo_on_remote(repo_name)
 
 
 if __name__ == "__main__":
