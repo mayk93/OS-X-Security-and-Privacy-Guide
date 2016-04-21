0fefa2e828d743597ac50d1c14a36b362e7a86b5
mayk93 <mihai.mandrescu@gmail.com>
2016-04-22 00:02:59 +0100

    Working cloning

--- daily_commit.py
+++ daily_commit.py
@@ -15,13 +15,14 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Libs
+import os
 import random
 import github
 # -----
 
 
 # Constants
-LANGUAGES = ["python", "python", "python", "go", "javascript"]
+LANGUAGES = ["python", "python", "python", "go", "javascript"]  # More python to increase python probability
 
 
 def choose_repo(result):
@@ -29,19 +30,26 @@ def choose_repo(result):
     number = random.choice([i for i in range(10, 100)])
     for index, repo in enumerate(result):
         if index > number and letter in repo.clone_url:
-            return repo.clone_url
-    return result[number].clone_url
+            return repo.clone_url, repo.name
+    return result[number].clone_url, result[number].name
 
 
 def get_query():
     return "language:" + random.choice(LANGUAGES)
 
 
+def clone(url, name):
+    logging.info("Cloning " + name + " from " + url)
+    os.system("mkdir ClonedRepos/" + name)
+    os.system("cd ClonedRepos/" + name + "; git clone " + url)
+
+
 def main():
     client = github.Github()
     result = client.search_repositories(query=get_query(), sort="stars", order="desc")
-    repo_url = choose_repo(result)
-    logging.info("Now cloning: " + unicode(repo_url))
+    repo_url, repo_name = choose_repo(result)
+    clone(repo_url, repo_name)
+
 
 if __name__ == "__main__":
     main()
